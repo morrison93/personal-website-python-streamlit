94c9b7d8e91aca383aefea96b696b974637bce21
unknown <rodrigo.rocha582@gmail.com>
2024-04-14 16:23:59 +0200

    updated text info in app.py

--- app.py
+++ app.py
@@ -30,32 +30,30 @@ img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
 # ---- HEADER SECTION ----
 with st.container():
-    st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
+    st.subheader("Hi, I am Rodrigo :wave:")
+    st.title("A Systems Engineer From Portugal!")
     st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+        "I am passionate about technology and exploring new ways of applying my knowledge to real life problems and business settings."
     )
-    st.write("[Learn More >](https://pythonandvba.com)")
 
 # ---- WHAT I DO ----
 with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("What I do")
+        st.header("Who Am I?")
         st.write("##")
         st.write(
             """
-            On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
+            I'm a Telecommunication and Informatics Engineer graduated in Lisbon, Portugal in 2019 specializing in service management, network and security. I'm currently living in Toulouse, France since 2021 and this webpage is mainly too:
+            - Leverage my technical skills by building small projects and trying to exercise some skills that I haven't acquired or I'm still in the process of acquiring.
+            - Present myself to the world through this small project.
+            - Keep a track of my career by hopefully keeping this webpage up to date.
+            - Make connections throughout the world regarding projects or some other interesting topic.
 
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            If this sounds interesting to you, please consider to contact me though the "Get In Touch With Me!" section.
             """
         )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
     with right_column:
         st_lottie(lottie_coding, height=300, key="coding")
 
@@ -63,7 +61,7 @@ with st.container():
 with st.container():
     st.write("---")
     st.header("My Projects")
-    st.write("##")
+    st.write("In this section, you can check small projects/exercises that I do from time to time in order to upgrade my skills or trying some take on solutions for problems that I thought of. In this spirit I will sometimes provide the context of the so called project/exercise with some notes if needed and usually followed by some reference (github link or other).")
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_lottie_animation)
@@ -99,7 +97,7 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+    <form action="https://formsubmit.co/rodrigo.rocha582@gmail.COM" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
@@ -112,3 +110,16 @@ with st.container():
         st.markdown(contact_form, unsafe_allow_html=True)
     with right_column:
         st.empty()
+
+with st.container():
+    st.write("---")
+    st.header("About this webpage")
+    st.write("##")
+    st.write("""
+            If you like this page I'm obliged to say, that I hate pretty much every aspect of front-end development and I can say quite confidently that I am prety awful in it too.
+            I was looking for a fast solution for making a fast presentation webpage and I stumbled upon Sven's youtube channel and I also forked the repo that he made available.
+            Let it be known that I intend to iterate upon this webpage and it will distantiate itself from Sven's solution in time.
+            Being this said, I will leave the links for his youtube channel and the repository that gave origin to this webpage. 
+            """)
+    st.write("[Sven's YouTube Channel](https://youtube.com/c/CodingIsFun)")
+    st.write("[Sven's Streamlit Repo](https://github.com/Sven-Bo/personal-website-streamlit)")
